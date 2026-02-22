1cfe0d3ea951432b6e7f5e4eb0fe07987567143a
PRADEEPP2008 <2008pradeepp@gmail.com>
2026-02-22 22:51:40 +0530

    Add files via upload

---
+++ OOPSBannerAppUC5.java
@@ -0,0 +1,54 @@
+public class OOPSBannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                String.join("   ",
+                        "  *****  ",
+                        "  *****  ",
+                        "  *****  ",
+                        "  *****  "),
+
+                String.join("   ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "),
+
+                String.join("   ",
+                        " *     * ",
+                        " *     * ",
+                        " *     * ",
+                        " *       "),
+
+                String.join("   ",
+                        " *     * ",
+                        "  *****  ",
+                        "  *****  ",
+                        "  *****  "),
+
+                String.join("   ",
+                        " *     * ",
+                        " *       ",
+                        "      *  ",
+                        "       * "),
+
+                String.join("   ",
+                        " *     * ",
+                        " *       ",
+                        "      *  ",
+                        "       * "),
+
+                String.join("   ",
+                        "  *****  ",
+                        " *       ",
+                        "  *****  ",
+                        "  *****  ")
+        };
+
+        // Enhanced for loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
